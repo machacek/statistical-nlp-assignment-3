ed9f6e9426c3340c9e31dce18135900770e3ed93
Matous Machacek <machacekmatous@gmail.com>
2014-05-28 16:14:39 +0200

    working on second part

--- part2/hmm.py
+++ part2/hmm.py
@@ -1,47 +1,109 @@
 #!/usr/bin/env python3
-from collection import namedtuple
+from collection import namedtuple, Counter
+from math import log2
+from itertools import zip_longest
 
 class SupervisedHMMTagger(object):
     def __init__(self, train_data, n=3):
-        self.n = n
+        self.n = n 
+        self.lambdas = (n+1) * (1,) # pocatecni parametry vylazovani, pozor, parametry jsou v obracenem poradi
+        self.c_tw = Counter() # pocet videnycn dvojic tag, slovo (c_wt ze slidu)
+        self.c_t  = Counter() # pocet videnych tagu (c_t ze slidu)
+        self.c_h  = Counter() # pocet videnych historii tagu (c_t(n-1) ze slidu)
+        self.c_ht = Counter() # pocet videnych tag n-gramu (c_tn ze slidu)
+        
+
+
+
+
+    def decode_sentence(self, sentence):
+        words = list(sentence)
+
+        # Viterbi algorithm
+        TrelisNode = namedtuple("TrelisNode", ["log_alpha","previous_node","tag"])
+        stage = { self.start_state() : TrelisNode(log_alpha=0, previous=None, tag=None) }
+        for word in words:
+            new_stage = {}
+            for previous_state, previous_node in stage.items():
+                for tag in self.possible_tags(word):
+
+                    log_alpha = previous_node.log_alpha \
+                            + self.log_tag_probability(tag, previous_state) \
+                            + self.log_word_probability(word, tag)
+
+                    state = previous_state[1:] + (tag,)
+
+                    if state not in new_stage_indexed or new_stage_indexed[state].log_alpha < log_alpha:
+                        new_node = TrelisNode(log_alpha=log_alpha, previous=previous_node, tag=tag)
+                        new_stage_indexed[state] = new_node
+
+            stage = new_stage
+
+        # Backtrace the best tag path
+        node = max(stage.values(), key=lambda x: x.log_alpha)
+        rev_tags = []
+        while node.tag is not None:
+            rev_tags.append(node.tag)
+            node = node.previous_node
+        return reversed(rev_tags)
+
+    def train_file(self, file):
+        for line in file:
+            sentence = [item.split('/',2) for item in line.split()]
+            self.train_sentence(sentence)
 
     def train_sentence(self, sentence):
         sentence = list(sentence)
         words = [word for word, tag in sentence]
         tags = [tag for word, tag in sentence]
-        for word, tag, tag_n_gram in zip(words, tags, n_gram_generator):
-            pass
+        for word, tag, tag_history in zip(words, tags, self.n_grams(tags)):
+            self.c_tw[tag,word] += 1
+            self.c_t[tag] += 1
+            for suffix in suffixes(tag_history):
+                self.c_h[suffix] += 1
+                self.c_ht[suffix, tag] += 1
 
-
-    def tag(self, data_to_tag):
+    def possible_tags(self, word):
         pass
 
-    def viterbi(self, data_to_tag):
+    def number_of_possible_words(self, tag=None):
+        pass
 
-        TrelisNode = namedtuple("TrelisNode", ["state","probability","previous"])
+    def tag_probability(self, tag, tag_history):
+        sum = 0
+        for lambda_coeff, suffix in safe_zip(self.lambdas[:-1], suffixes(tag_history)):
+            sum += lambda_coeff * self.c_h[suffix] / self.c_ht(suffix, tag)
+        sum += self.lambdas[-1] / self.number_of_possible_words()
+        return sum
 
-        stage = [TrelisNode(state=self.get_start_state(), probability=1, previous=None)]
-        for word in data_to_tag:
-            new_stage = []
-            
-            for node in stage:
-           
+    def word_probability(self, word, tag):
+        nominator = self.c_tw[tag, word] + 1
+        denominator = self.c_t[tag] + self.number_of_possible_words(tag)
+        return nominator / denominator
 
-    def get_start_state(self):
-        return (None,) * self.get_n()
+    def log_tag_probability(self, tag, tag_history):
+        return log2(self.tag_probability(tag, tag_history))
 
-    def get_n(self):
-        return self.n
+    def log_word_probability(self, word, tag):
+        return log2(self.log_word_probability(word, tag))
 
-def n_grams(iterable, n=2):
-    iterator = iter(iterable)
-    actual_n_gram = deque(None for _ in range(n))
+    def start_state(self):
+        return (self.n - 1) * (None,)
 
-    # fill the first n items to deque
-    for _ in range(n):
-        actual_n_gram.append(next(iterator))
+    def n_grams(iterable):
+        iterator = iter(iterable)
+        n_gram = deque(self.start_state())
+        while True:
+            yield tuple(n_gram)
+            n_gram.popleft()
+            n_gram.append(next(iterator))
 
-    while True:
-        actual_n_gram.popleft()
-        actual_n_gram.append(next(iterator))
-        yield tuple(actual_n_gram)
+def safe_zip(*args):
+    for n_tuple in zip_longest(*args):
+        assert None not in n_tuple
+        yield n_tuple
+
+def suffixes(sequence):
+    for i in range(len(sequence) + 1):
+        yield sequence[i:]
+
