145efddd29765ae333982514bd5c03353e83beb5
Matous Machacek <machacekmatous@gmail.com>
2014-06-02 10:57:59 +0200

    hopefully working unsupervised learning

--- hmm.py
+++ hmm.py
@@ -17,13 +17,26 @@ class HMMTagger(object):
         self.word_lexicon = defaultdict(set)
         self.tag_lexicon = defaultdict(set)
 
-    def train_parameters(self, train_data):
-        for sentence in train_data:
-            self.train_parameters_sentence(sentence)
+    def concat_labeled_sentences(self, sentences):
+        first = True
+        for sentence in sentences:
+            if not first:
+                yield from [(None, None) for _ in self.start_state()]
+            first = False
+            yield from sentence
+    
+    def concat_unlabeled_sentences(self, sentences):
+        first = True
+        for sentence in sentences:
+            if not first:
+                yield from self.start_state()
+            first = False
+            yield from sentence
 
-    def train_parameters_sentence(self, sentence):
-        words = [word for word, tag in sentence]
-        tags = [tag for word, tag in sentence]
+    def train_parameters(self, sentences):
+        train_data = list(self.concat_labeled_sentences(sentences))
+        words = [word for word, tag in train_data]
+        tags = [tag for word, tag in train_data]
         for word, tag, tag_history in zip(words, tags, self.history_generator(tags)):
             self.c_tw[tag,word] += 1
             self.c_t[tag] += 1
@@ -32,9 +45,96 @@ class HMMTagger(object):
                 self.c_ht[suffix, tag] += 1
             self.word_lexicon[word].add(tag)
             self.tag_lexicon[tag].add(word)
+    
+    def train_unlabeled(self, unlabeled_sentences):
+        unlabeled_data = list(self.concat_unlabeled_sentences(unlabeled_sentences))
 
-    def train_lambdas(self, held_out_data):
-        held_out_data = list(held_out_data)
+        # Forward-Backward algorithm
+        for i in range(1):
+            print("\nForward-Backward algorithm - iteration %s" % i, file=sys.stderr)
+
+            tag_word_counts = defaultdict(negative_infinity)
+            tag_counts = defaultdict(negative_infinity)
+            history_tag_counts = defaultdict(negative_infinity)
+            history_counts = defaultdict(negative_infinity)
+
+            # Compute forward probabilities (alphas)
+            print("Computing forward probabilities", file=sys.stderr)
+            stages = [{ self.start_state() : ForwardBackwardTrelisNode(log_alpha=0) }]
+            for word in unlabeled_data:
+                new_stage = defaultdict(ForwardBackwardTrelisNode)
+                
+                for tag in self.possible_tags(word):
+                    for previous_state, previous_node in stages[-1].items():
+                        # Computint alpha increase
+                        log_alpha_inc = previous_node.log_alpha \
+                                + self.log_tag_probability(tag, previous_state) \
+                                + self.log_word_probability(word, tag)
+                        
+                        # Increasing the alpha
+                        new_state = previous_state[1:] + (tag,)
+                        new_stage[new_state].log_inc_alpha(log_alpha_inc)
+
+                # Adding the new stage to the list
+                stages.append(new_stage)
+
+
+            # Compute backward probabilities (betas)
+            print("Computing backward probabilities", file=sys.stderr)
+            for node in stages[-1].values():
+                node.log_beta=0
+            for t, word in reversed(list(enumerate(unlabeled_data))):
+                current_stage = stages[t]
+                next_stage = stages[t+1]
+                for tag in self.possible_tags(word):
+                    for state, node in current_stage.items():
+
+                        # Getting next stage node for given tag, to which there is an edge from actual node
+                        next_state = state[1:] + (tag,)
+                        next_node = next_stage[next_state]
+
+                        # Computing beta increase
+                        log_beta_inc = next_node.log_beta \
+                                + self.log_word_probability(word, tag) \
+                                + self.log_tag_probability(tag, state)
+
+                        node.log_inc_beta(log_beta_inc)
+            
+            
+
+            # Accumulate the counts
+            print("Accumulating counts", file=sys.stderr)
+            for t, word in enumerate(unlabeled_data, 1):
+                for tag in self.possible_tags(word):
+                    for previous_state, previous_node in stages[t-1].items():
+                        state = previous_state[1:] + (tag,)
+                        node = stages[t][state]
+                        
+                        log_expected_count_inc = \
+                                  previous_node.log_alpha \
+                                + self.log_tag_probability(tag, previous_state) \
+                                + self.log_word_probability(word, tag) \
+                                + node.log_beta
+
+                        tag_word_counts[tag,word] = log_add(tag_word_counts[tag,word], log_expected_count_inc)
+                        tag_counts[tag] = log_add(tag_counts[tag], log_expected_count_inc)
+
+                        history = previous_state
+                        for suffix in suffixes(history):
+                            history_tag_counts[suffix, tag] = log_add(history_tag_counts[suffix, tag], log_expected_count_inc)
+                            history_counts[suffix] = log_add(history_counts[suffix], log_expected_count_inc)
+            
+            data_log_prob = log_sum(node.log_alpha for node in stages[-1].values())
+            print("Data log probability: %s" % data_log_prob, file=sys.stderr)
+
+            # Substitute current counts
+            self.tag_word_counts = tag_word_counts
+            self.tag_counts = tag_counts
+            self.history_tag_counts = history_tag_counts
+            self.history_counts = history_counts
+
+    def train_lambdas(self, sentences):
+        held_out_data = list(self.concat_labeled_sentences(sentences))
         
         epsilon = 0.001
 
@@ -52,13 +152,12 @@ class HMMTagger(object):
             # Prepare list of lambda multiplicators
             lambdas = [0 for _ in self.lambdas]
 
-            for sentence in held_out_data:
-                tags = [tag for word,tag in sentence]
-                for tag, tag_history in zip(tags, self.history_generator(tags)):
-                    interpolated_prob = self.tag_probability(tag, tag_history)
-                    for i, suffix in enumerate(suffixes(tag_history)):
-                        lambdas[i] += self.n_tag_probability(tag, suffix) / interpolated_prob
-                    lambdas[-1] +=  1 / (self.vocabulary_size() * interpolated_prob)
+            tags = [tag for word,tag in held_out_data]
+            for tag, tag_history in zip(tags, self.history_generator(tags)):
+                interpolated_prob = self.tag_probability(tag, tag_history)
+                for i, suffix in enumerate(suffixes(tag_history)):
+                    lambdas[i] += self.n_tag_probability(tag, suffix) / interpolated_prob
+                lambdas[-1] +=  1 / (self.vocabulary_size() * interpolated_prob)
             
             # Multiply with old lambdas and normalize
             lambdas = [lambda_ * lambda_mul for lambda_, lambda_mul in zip(self.lambdas, lambdas)]
@@ -78,16 +177,6 @@ class HMMTagger(object):
             print("New Cross Entropy:", self.tag_cross_entropy(held_out_data), file=sys.stderr)
 
         print("End of EM Smoothing Algorithm", file=sys.stderr)
-
-    def tag_cross_entropy(self, held_out_data):
-        sum = 0
-        count = 0
-        for sentence in held_out_data:
-            tags = [tag for word,tag in sentence]
-            for tag, tag_history in zip(tags, self.history_generator(tags)):
-                sum += self.log_tag_probability(tag, tag_history)
-                count += 1
-        return -sum / count
 
     def decode(self, data):
         for sentence in data:
@@ -128,13 +217,20 @@ class HMMTagger(object):
             node = node.previous_node
         return list(zip(words,reversed(rev_tags)))
 
-    
+    def tag_cross_entropy(self, held_out_data):
+        sum = 0
+        count = 0
+        tags = [tag for word,tag in held_out_data]
+        for tag, tag_history in zip(tags, self.history_generator(tags)):
+            sum += self.log_tag_probability(tag, tag_history)
+            count += 1
+        return -sum / count
+
     def possible_tags(self, word):
         tags = self.word_lexicon[word]
         if tags:
             return tags
         else:
-            #print("Unknown word: %s" % word, file=sys.stderr)
             return self.tag_lexicon.keys()
 
     def vocabulary_size(self, tag=None):
@@ -176,7 +272,7 @@ class HMMTagger(object):
 
     def n_tag_probability(self, tag, suffix):
         try:
-            return self.history_tag_expected_counts[suffix, tag] / self.history_expected_counts[suffix]
+            return 2**(self.history_tag_counts[suffix, tag] - self.history_counts[suffix])
         except AttributeError:
             try:
                 return self.c_ht[suffix, tag] / self.c_h[suffix] 
@@ -185,117 +281,9 @@ class HMMTagger(object):
 
     def word_probability(self, word, tag):
         try:
-            nominator = self.tag_word_expected_counts[tag,word]
-            denominator = self.tag_expected_counts[tag]
-            return nominator / denominator
+            return 2**(self.tag_word_counts[tag,word] - self.tag_counts[tag])
         except AttributeError:
-            nominator = self.c_tw[tag, word] + 1
-            denominator = self.c_t[tag] + self.vocabulary_size()
-            return nominator / denominator
-
-    def train_unlabeled(self, unlabeled_sentences):
-        unlabeled_sentences = list(unlabeled_sentences)
-
-
-        # Forward-Backward algorithm
-        for i in range(10):
-            print("\n\nForward-Backward algorithm - iteration %s" % i, file=sys.stderr)
-
-            tag_word_expected_counts = Counter()
-            tag_expected_counts = Counter()
-            history_tag_expected_counts = Counter()
-            history_expected_counts = Counter()
-
-            corpus_log_prob = 0
-
-            for sentence in unlabeled_sentences:
-                # print('.', end='', file=sys.stderr)
-                sys.stderr.flush()
-
-                # Compute forward probabilities (alphas)
-                stages = [{ self.start_state() : ForwardBackwardTrelisNode(log_alpha=0) }]
-                for word in sentence:
-                    new_stage = defaultdict(ForwardBackwardTrelisNode)
-                    
-                    for tag in self.possible_tags(word):
-                        for previous_state, previous_node in stages[-1].items():
-                            # Computint alpha increase
-                            log_alpha_inc = previous_node.log_alpha \
-                                    + self.log_tag_probability(tag, previous_state) \
-                                    + self.log_word_probability(word, tag)
-                            
-                            # Increasing the alpha
-                            new_state = previous_state[1:] + (tag,)
-                            new_stage[new_state].log_inc_alpha(log_alpha_inc)
-
-                    # Adding the new stage to the list
-                    stages.append(new_stage)
-
-
-                # Compute backward probabilities (betas)
-                for node in stages[-1].values():
-                    node.log_beta=0
-                for t, word in reversed(list(enumerate(sentence))):
-                    current_stage = stages[t]
-                    next_stage = stages[t+1]
-                    for tag in self.possible_tags(word):
-                        for state, node in current_stage.items():
-
-                            # Getting next stage node for given tag, to which there is an edge from actual node
-                            next_state = state[1:] + (tag,)
-                            next_node = next_stage[next_state]
-
-                            # Computing beta increase
-                            log_beta_inc = next_node.log_beta \
-                                    + self.log_word_probability(word, tag) \
-                                    + self.log_tag_probability(tag, state)
-
-                            node.log_inc_beta(log_beta_inc)
-                
-                
-                # Checkpoints
-                # for i, stage in enumerate(stages):
-                #     print(i, reduce(log_add, (node.log_alpha + node.log_beta for node in stage.values())), file=sys.stderr)
-                sentence_log_prob = reduce(log_add, (node.log_alpha for node in stages[-1].values()))
-                corpus_log_prob += sentence_log_prob
-                print("Sentence log probability: %s" % sentence_log_prob, file=sys.stderr)
-
-                # Accumulate the counts
-                for t, word in enumerate(sentence, 1):
-                    for tag in self.possible_tags(word):
-                        for previous_state, previous_node in stages[t-1].items():
-                            state = previous_state[1:] + (tag,)
-                            node = stages[t][state]
-
-                            #print(previous_node.log_alpha, node.log_beta, file=sys.stderr)
-                            
-                            log_expected_count_inc = \
-                                      previous_node.log_alpha \
-                                    + self.log_tag_probability(tag, previous_state) \
-                                    + self.log_word_probability(word, tag) \
-                                    + node.log_beta
-
-                            # print(log_expected_count_inc, file=sys.stderr)
-
-                            tag_word_expected_counts[tag,word] += 2**(log_expected_count_inc / len(sentence))
-                            tag_expected_counts[tag] += 2**(log_expected_count_inc / len(sentence))
-
-                            history = previous_state
-                            for suffix in suffixes(history):
-                                history_tag_expected_counts[suffix, tag] += 2**(log_expected_count_inc / len(sentence))
-                                history_expected_counts[suffix] += 2**(log_expected_count_inc / len(sentence))
-
-            # Substitute current counts
-            self.tag_word_expected_counts = tag_word_expected_counts
-            self.tag_expected_counts = tag_expected_counts
-            self.history_tag_expected_counts = history_tag_expected_counts
-            self.history_expected_counts = history_expected_counts
-
-            tag_word_sums = Counter()
-            history_tag_sums = Counter()
-
-            print("Corpus log probability: %s" % corpus_log_prob, file=sys.stderr)
-
+            return (self.c_tw[tag, word] + 1) / (self.c_t[tag] + self.vocabulary_size())
 
                 
 class ViterbiTrelisNode(object):
@@ -323,6 +311,9 @@ class ForwardBackwardTrelisNode(object):
     def log_inc_beta(self, log_beta_addition):
         self.log_beta = log_add(self.log_beta, log_beta_addition)
 
+def negative_infinity():
+    return float('-inf')
+
 def safe_zip(*args):
     for n_tuple in zip_longest(*args):
         assert None not in n_tuple
@@ -332,7 +323,7 @@ def suffixes(sequence):
     for i in range(len(sequence) + 1):
         yield sequence[i:]
 
-log_big = log2(10**30)
+log_big = 100
 def log_add(x,y):
     if x == float('-inf') and y == float('-inf'):
         return y
@@ -343,3 +334,6 @@ def log_add(x,y):
     else:
         _min = min(x,y)
         return _min + log2(2**(x - _min) + 2**(y - _min))
+
+def log_sum(iterable):
+    return reduce(log_add, iterable)
